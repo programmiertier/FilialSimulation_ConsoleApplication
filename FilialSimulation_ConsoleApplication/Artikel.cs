5b2caba50e73119047a8b516e6cb932289322189
programmiertier <zimmermannveronique@protonmail.com>
2017-10-17 09:43:24 +0200

    Klassen eigens angelegt. Klasse Regal bis hierhin aufgenommen

---
+++ FilialSimulation_ConsoleApplication/Artikel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using static System.Console;
+
+namespace FilialSimulation_ConsoleApplication
+{
+    class Artikel
+    {
+        int id;
+        double volumen;
+        double preis;
+
+        public Artikel(int meineid, Artikel[] warenkorb)
+        {
+            WriteLine("Artikel {0} angelegt: ", warenkorb[meineid].id);
+            id = warenkorb[meineid].id;
+            volumen = warenkorb[meineid].volumen;
+            preis = warenkorb[meineid].preis;
+            WriteLine("Mit einem Preis von {0:F2} und einem Volumen von {1:F2}", preis, volumen);
+        }
+
+
+        public Artikel(int meineid, double meinpreis, double meinvolumen)
+        {
+            // WriteLine("Artikel {0} angelegt: ", id);
+            id = meineid;
+            volumen = meinvolumen;
+            preis = meinpreis;
+        }
+
+
+    }
+}
---
+++ FilialSimulation_ConsoleApplication/Regal.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using static System.Console;
+
+namespace FilialSimulation_ConsoleApplication
+{
+    public class Regal
+    {
+        int id;
+        double volumenRegal = 1.0;
+        int aktuellerInhalt;
+        double aktuellerWert;       // wie viel die aktuellen Waren, im Regal (natürlich... Peperoni) wert sind
+        double kapazitaet;
+        bool nachfuellen;
+        double mindestBestand;
+        
+
+        public void mindest(double mindestBestand, double kapazitaet)
+        {
+            mindestBestand = kapazitaet * 0.3;
+        }
+    }
+}
